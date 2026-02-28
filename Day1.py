654dacfd63da1fc4f4ccd062d43e849a2536b7cc
Garima Kanwar <garimakanwar.sbcet@gmail.com>
2026-02-28 20:49:36 +0530

    Day 1 - Python Basics

---
+++ Day1.py
@@ -0,0 +1,41 @@
+print("Hello, World!")
+
+import sys
+print(sys.version)
+
+#indentation
+#python uses indentation to define blocks of code.
+
+if 3>2:
+    print("Three is greater than two!")
+
+#python variables
+x = 5
+y = "Hello, World!"
+
+#comments
+#This is a comment.
+print("I have learned how to check version, use indentation, comments and variables in Python!")
+
+#statements
+print("Programming instructions are called statements")
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+
+#Semicolons in python
+#In Python, semiccolons are not required to end statements, but they can be used to seperate multiple statements on a simngle line.
+print("Hello"); print("World"); print("This is Python!")
+
+print("Hello World", end=" ")
+print("I will print on the same line.")
+#end parameter in print function is used to specify what to print at the end of the output. By default, it is a newline character (\n), but you can change it to a space or any other specified string. In this example, we set end=" " to print a space instead of a newline, allowing the next print statement to continue on the same line.
+
+#print numbers
+print(10)
+#we dont put numbers inside doble quotes.
+print(8+10)
+
+#mix text and mumbers
+print("I am", 21, "years old.")
